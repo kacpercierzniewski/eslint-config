3dd3abb1827386e1c7748a14481b43bfbab7faac
Kacper Cierzniewski <kacper.cierzniewski@synergycodes.com>
2021-10-16 21:18:07 +0200

    my own eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    semi: [2, 'never'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    quotes: [2, 'single'],
+    'react/prop-types': [0, {}],
+  },
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# `@peleek/eslint-config`
+
+> TODO: description
+
+## Usage
+
+```
+const eslintConfig = require('@peleek/eslint-config');
+
+// TODO: DEMONSTRATE API
+```
